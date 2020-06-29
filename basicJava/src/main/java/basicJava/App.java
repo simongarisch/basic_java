0f7a2eca084b288e230359632e791470cc49814f
simongarisch <gatman946@gmail.com>
2020-06-29 12:22:55 +1000

    clean

--- basicJava/src/main/java/basicJava/App.java
+++ basicJava/src/main/java/basicJava/App.java
@@ -1,10 +1,7 @@
 package basicjava;
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
