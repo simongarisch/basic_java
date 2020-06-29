9a175970be3ae8d08095863a17bada2f61d5f4b4
simongarisch <gatman946@gmail.com>
2020-06-29 14:10:17 +1000

    adding to tests

--- basicJava/src/test/java/basicJava/AppTest.java
+++ basicJava/src/test/java/basicJava/AppTest.java
@@ -1,14 +1,20 @@
 package basicjava;
 
+import csvop.CsvReader;
+import java.util.ArrayList;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue(){
         assertTrue( true );
+    }
+
+    @Test
+    public void testCsvReader(){
+        ArrayList<String[]> contents = CsvReader.Read();
+        assertTrue(contents.size() == 3);
     }
 }
